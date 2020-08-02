3d4690837bf781f89b8755b27cfc320c286c7ac5
Ryan Wojtyla <ryan@droplit.io>
2020-08-01 17:09:34 -0700

    very base proof on concept complete

--- json2Html.hs
+++ json2Html.hs
@@ -1,3 +1,5 @@
+-- Read a JSON recipe
+
 -- enables us to use generics to convert JSON to our custom datatype
 {-# LANGUAGE DeriveDataTypeable #-}
 
@@ -13,15 +15,38 @@ import Text.JSON.Generic
 
 -- store JSON data in Haskell data structure
 --data Recipie = Recipie { title :: String } deriving (Show, Data, Typeable)
-import Recipie
+import Recipe
 
 -- read JSON file and populate data structure
-getJSON :: String -> Recipie
+getJSON :: String -> Recipe
 getJSON s = decodeJSON s 
 
+-- address each section of the JSON individually
+seperateJSON :: Recipe -> String
+seperateJSON (Recipe title source ings steps) = renderHtml (getTitle title) ++
+                                                renderHtml (getSource source) ++
+                                                renderHtml (getIngs ings) ++
+                                                renderHtml (getSteps steps)
+
 -- produce the title in HTML
-getTitle :: Recipie -> Html
-getTitle (Recipie title) = h1 (stringToHtml title)
+getTitle :: String -> Html
+getTitle t = h1 (stringToHtml t)
+
+-- produce the source in HTML
+getSource :: String -> Html
+getSource s = h2 (stringToHtml s)
+
+-- produce the ingredients in HTML
+getIngs :: [Ingredient] -> Html
+getIngs is = unordList (map stringToHtml (map catIngPts is))
+
+-- concatenate the two parts of an Ingredient into one String
+catIngPts :: Ingredient -> String
+catIngPts (Ingredient item quan) = quan ++ " - " ++ item
+
+-- product the steps in HTML
+getSteps :: [String] -> Html
+getSteps ss = ordList ss
 
 main :: IO()
-main = interact $ renderHtml . getTitle . getJSON
+main = interact $ seperateJSON . getJSON
